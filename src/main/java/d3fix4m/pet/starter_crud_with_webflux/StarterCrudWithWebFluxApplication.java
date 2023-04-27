c3602674c085f041b1587deeb536ca3fb3170d16
MaxiType <48544955+MaxiType@users.noreply.github.com>
2023-04-27 17:44:07 +0300

    Initial commit

---
+++ src/main/java/d3fix4m/pet/starter_crud_with_webflux/StarterCrudWithWebFluxApplication.java
@@ -0,0 +1,13 @@
+package d3fix4m.pet.starter_crud_with_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterCrudWithWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterCrudWithWebFluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/d3fix4m/pet/starter_crud_with_webflux/StarterCrudWithWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package d3fix4m.pet.starter_crud_with_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterCrudWithWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
